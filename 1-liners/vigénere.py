69aea77878752da723d618a8de75be27d712e4cf
Florian Fuchs <58077515+Floplosion05@users.noreply.github.com>
2022-08-07 23:49:12 +0200

    Update vigénere.py

--- 1-liners/vigénere.py
+++ 1-liners/vigénere.py
@@ -3,12 +3,9 @@ def v_enc(k,m):
 	k*=int(len(m) / len(k)) + (len(m) % len(k) > 0)
 	for i,c in enumerate(m):
 		if ord('A')<=ord(c)<=ord('Z') or ord('a')<=ord(c)<=ord('z'):
-			print(c)
 			if ord('z')-ord(c.lower()) >= ord(k[i].lower())-ord('a'): #shift forward
-				print("1.")
 				r+=chr(ord(c.lower())+ord(k[i])-ord('a'))
 			elif ord(c.lower())-ord('a') >= ord(k[i])-ord('a'): #shift backward
-				print("2.")
 				r+=chr((ord('a'))-1+(ord(k[i])-ord('a'))-(ord('z')-ord(c.lower())))
 		else: #dont shift
 			r+=c
