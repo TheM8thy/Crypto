d8dbc41d58fbffb57b38d9fab4503859f8b7ce38
Floplosion05 <florianfuchs2005@web.de>
2022-08-07 16:00:08 +0200

    fixed mistakes

--- 1-liners/rot13.py
+++ 1-liners/rot13.py
@@ -1,1 +1,14 @@
-print((lambda m:''.join([[c,chr(ord(c)-13)]['n'<=c.lower()<'z'],chr(ord(c)+13)]['a'<=c.lower()<='m']for c in m))("VybirClgu0a"))
+print((lambda m:''.join([[c,chr(ord(c)-13)]['n'<=c.lower()<='z'],chr(ord(c)+13)]['a'<=c.lower()<='m'] for c in m))("VybirClgu0a"))
+
+def s(m):
+    r=""
+    for c in m:
+        if 'a'<=c.lower()<='m':
+            r+=chr(ord(c)+13)
+        elif 'n'<=c.lower()<='z':
+            r+=chr(ord(c)-13)
+        else:
+            r+=c
+    return r
+
+print(s("VybirClgu0a"))
--- 1-liners/vigénere.py
+++ 1-liners/vigénere.py
@@ -1,25 +1,29 @@
-m = "yyyy"
-k = "cbcb"
+m = "GREIFTDIEGALLIERAN"
+k = "DINGSBIBERICH"
 r = ""
+a=0
+
+k*=int(len(m) / len(k)) + (len(m) % len(k) > 0)
 
 for i,c in enumerate(m):
-    #print(i,c,k[i])
-    #print("dist z: ",ord('z')-ord(c.lower()))
-    #print("dist a: ",ord(k[i])-ord('a'))
-    if ord('z')-ord(c.lower()) >= ord(k[i])-ord('a'): #shift forward
+    print(ord('z')-ord(c.lower()),">",ord(k[i].lower())-ord('a'))
+    if ord('z')-ord(c.lower()) >= ord(k[i].lower())-ord('a'): #shift forward
 
+        print("1.",c,k[i],ord(c.lower()),ord(k[i])-ord('A'))
         r+=chr(ord(c.lower())+ord(k[i])-ord('a'))
-        print ("1.")
 
     elif ord(c.lower())-ord('a') >= ord(k[i])-ord('a'): #shift backward
 
-        #r+=chr(ord(k[i])-(ord('z')-ord(c.lower())+1))
-        r+=chr(ord(k[i])-(ord('z')-ord(c.lower()))+(ord(k[i])-ord('a')))
-        print ("2.")
+        print("2.")
+        #print(c.lower(),": C-Dist to z:",ord('z')-ord(c.lower()))
+        #print(k[i],": K-Dist to a:",ord(k[i])-ord('a'))
+        a=(ord(k[i])-ord('a'))-(ord('z')-ord(c.lower()))
+        print("Substraction:",(ord(k[i])-ord('a'))-(ord('z')-ord(c.lower())),"Letter:",chr((ord('a'))-1+a))
+        r+=chr((ord('a'))-1+(ord(k[i])-ord('a'))-(ord('z')-ord(c.lower())))
 
     else: #dont shift
         r+=c
 
-print(r)
+print("start:"+r+":end")
 
-print((lambda k,m:''.join([[c,chr(ord(k[i])-(ord('z')-ord(c.lower())+1))][ord(c.lower())-ord('a') >= ord(k[i])-ord('a')],chr(ord(c.lower())+ord(k[i])-ord('a'))][ord('z')-ord(c.lower()) >= ord(k[i])-ord('a')] for i,c in enumerate(m)))("yyyy","cbcb"))
+print((lambda k,m:(k*=int(len(m) / len(k)) + (len(m) % len(k) > 0)) or ''.join([[c,chr((ord('a'))-1+(ord(k[i])-ord('a'))-(ord('z')-ord(c.lower())))][ord(c.lower())-ord('a') >= ord(k[i])-ord('a')],chr(ord(c.lower())+ord(k[i])-ord('a'))][ord('z')-ord(c.lower()) >= ord(k[i].lower())-ord('a')] for i,c in enumerate(m)))("haha","haha"))
