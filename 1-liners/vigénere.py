dfb06320e4467559263df116211170bba0c53738
Florian Fuchs <58077515+Floplosion05@users.noreply.github.com>
2022-08-07 18:21:47 +0200

    Update vigénere.py

--- 1-liners/vigénere.py
+++ 1-liners/vigénere.py
@@ -1,23 +1,22 @@
-m = "a"
-k = "a"
-r = ""
-a=0
-k*=int(len(m) / len(k)) + (len(m) % len(k) > 0)
+m = "444"
+k = "n"
 
-for i,c in enumerate(m):
-	print(ord('z')-ord(c.lower()),">",ord(k[i].lower())-ord('a'))
-	if ord('z')-ord(c.lower()) >= ord(k[i].lower())-ord('a'): #shift forward
-		print("1.",c,k[i],ord(c.lower()),ord(k[i])-ord('A'))
-		r+=chr(ord(c.lower())+ord(k[i])-ord('a'))
-	elif ord(c.lower())-ord('a') >= ord(k[i])-ord('a'): #shift backward
-		print("2.")
-		#print(c.lower(),": C-Dist to z:",ord('z')-ord(c.lower()))
-		#print(k[i],": K-Dist to a:",ord(k[i])-ord('a'))
-		a=(ord(k[i])-ord('a'))-(ord('z')-ord(c.lower()))
-		print("Substraction:",(ord(k[i])-ord('a'))-(ord('z')-ord(c.lower())),"Letter:",chr((ord('a'))-1+a))
-		r+=chr((ord('a'))-1+(ord(k[i])-ord('a'))-(ord('z')-ord(c.lower())))
-	else: #dont shift
-		r+=c
-print("start:"+r+":end")
+def v(k,m):
+	r=""
+	k*=int(len(m) / len(k)) + (len(m) % len(k) > 0)
+	for i,c in enumerate(m):
+		if ord('A')<=ord(c)<=ord('Z') or ord('a')<=ord(c)<=ord('z'):
+			print(c)
+			if ord('z')-ord(c.lower()) >= ord(k[i].lower())-ord('a'): #shift forward
+				print("1.")
+				r+=chr(ord(c.lower())+ord(k[i])-ord('a'))
+			elif ord(c.lower())-ord('a') >= ord(k[i])-ord('a'): #shift backward
+				print("2.")
+				r+=chr((ord('a'))-1+(ord(k[i])-ord('a'))-(ord('z')-ord(c.lower())))
+		else: #dont shift
+			r+=c
+	return r
+	
+print("start:"+v("n","444")+":end")
 
-print((lambda k,m:'start:'+''.join([[c,chr((ord('a'))-1+(ord(k[i])-ord('a'))-(ord('z')-ord(c.lower())))][ord(c.lower())-ord('a') >= ord(k[i])-ord('a')],chr(ord(c.lower())+ord(k[i])-ord('a'))][ord('z')-ord(c.lower()) >= ord(k[i].lower())-ord('a')] for i,c in enumerate(m))+(':end'))('haha','aaaa'))
+print((lambda k,m:'start:'+''.join([c,[[c,chr((ord('a'))-1+(ord((k*(int(len(m) / len(k)) + (len(m) % len(k) > 0)))[i])-ord('a'))-(ord('z')-ord(c.lower())))][ord(c.lower())-ord('a') >= ord((k*(int(len(m) / len(k)) + (len(m) % len(k) > 0)))[i])-ord('a')],chr(ord(c.lower())+ord((k*(int(len(m) / len(k)) + (len(m) % len(k) > 0)))[i])-ord('a'))][ord('z')-ord(c.lower()) >= ord((k*(int(len(m) / len(k)) + (len(m) % len(k) > 0)))[i].lower())-ord('a')]][ord('A')<=ord(c)<=ord('Z') or ord('a')<=ord(c)<=ord('z')] for i,c in enumerate(m))+(':end'))('abc','444'))
