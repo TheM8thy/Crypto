6ec7a013f16239724f83fd406660943b085c16e3
Florian Fuchs <58077515+Floplosion05@users.noreply.github.com>
2022-08-07 23:36:16 +0200

    Add files via upload

--- 1-liners/polybius.py
+++ 1-liners/polybius.py
@@ -1,15 +1,14 @@
-matrix = [['A','B','C','D','E'],['F','G','HI','J','K'],['L','M','N','O','P'],['Q','R','S','T','U'],['V','W','X','Y','Z']]
-m = "ABDF"
-r=''
+def p(matrix,m):
+	r=''
+	for c in m:
+	    for row in matrix:
+	        for column in row:
+	            if c in column:
+	                r+=str(row.index(column)+1)+str(matrix.index(row)+1)
+	                break
+	
+	return r
 
-for c in m:
-    for row in matrix:
-        for column in row:
-            if c in column:
-                print(str(row.index(column)+1)+str(matrix.index(row)+1)+' ')
-                r+=str(row.index(column)+1)+str(matrix.index(row)+1)+' '
-                break
-
-print(r)
+print("start:"+p([['A','B','C','D','E'],['F','G','HI','J','K'],['L','M','N','O','P'],['Q','R','S','T','U'],['V','W','X','Y','Z']],"ABDF")+":end")
 
 (lambda matrix,m:([[[print(str(row.index(column)+1)+str(matrix.index(row)+1)) for column in row if c in column] for row in matrix] for c in m]))([['A','B','C','D','E'],['F','G','HI','J','K'],['L','M','N','O','P'],['Q','R','S','T','U'],['V','W','X','Y','Z']],m = "ABDF")
