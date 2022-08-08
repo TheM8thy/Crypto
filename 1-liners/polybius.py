d012f124f5fe260906f04bdd59ff27309deffc37
Florian Fuchs <58077515+Floplosion05@users.noreply.github.com>
2022-08-08 17:29:20 +0200

    Update polybius.py

--- 1-liners/polybius.py
+++ 1-liners/polybius.py
@@ -6,9 +6,17 @@ def p_enc(matrix,m):
 	            if c in column:
 	                r+=str(row.index(column)+1)+str(matrix.index(row)+1)
 	                break
-	
 	return r
 
-print("start:"+p([['A','B','C','D','E'],['F','G','HI','J','K'],['L','M','N','O','P'],['Q','R','S','T','U'],['V','W','X','Y','Z']],"ABDF")+":end")
+def p_dec(matrix,c):
+	r=''
+	for n in [c[i:i+2] for i,v in enumerate(c) if i%2 == 0]:
+		r+=matrix[int(n[1])-1][int(n[0])-1]
+	return r
 
-(lambda matrix,m:([[[print(str(row.index(column)+1)+str(matrix.index(row)+1)) for column in row if c in column] for row in matrix] for c in m]))([['A','B','C','D','E'],['F','G','HI','J','K'],['L','M','N','O','P'],['Q','R','S','T','U'],['V','W','X','Y','Z']],m = "ABDF")
+print("start:"+p_enc([['A','B','C','D','E'],['F','G','H/I','J','K'],['L','M','N','O','P'],['Q','R','S','T','U'],['V','W','X','Y','Z']],"ABDF")+":end")
+print("start:"+p_dec([['A','B','C','D','E'],['F','G','H/I','J','K'],['L','M','N','O','P'],['Q','R','S','T','U'],['V','W','X','Y','Z']],"11214112")+":end")
+
+(lambda matrix,m:([[[print(str(row.index(column)+1)+str(matrix.index(row)+1),end='') for column in row if c in column] for row in matrix] for c in m]))([['A','B','C','D','E'],['F','G','H/I','J','K'],['L','M','N','O','P'],['Q','R','S','T','U'],['V','W','X','Y','Z']],m = "ABDF")
+print()#newline
+print("start:"+(lambda matrix,c:''.join(matrix[int(n[1])-1][int(n[0])-1] for n in [c[i:i+2] for i,v in enumerate(c) if i%2 == 0]))([['A','B','C','D','E'],['F','G','H/I','J','K'],['L','M','N','O','P'],['Q','R','S','T','U'],['V','W','X','Y','Z']],"11214112")+":end")
