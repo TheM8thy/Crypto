2f4fcb7c7b984607d243d161b37d541f5ad22b65
Florian Fuchs <58077515+Floplosion05@users.noreply.github.com>
2022-08-08 22:32:57 +0200

    Update rot13.py

--- 1-liners/rot13.py
+++ 1-liners/rot13.py
@@ -1,14 +1,14 @@
 def r(m):
-    r=""
-    for c in m:
-        if 'a'<=c.lower()<='m':
-            r+=chr(ord(c)+13)
-        elif 'n'<=c.lower()<='z':
-            r+=chr(ord(c)-13)
-        else:
-            r+=c
-    return r
+	r=""
+	for c in m:
+		if 'a'<=c.lower()<='m':
+			r+=chr(ord(c)+13)
+		elif 'n'<=c.lower()<='z':
+			r+=chr(ord(c)-13)
+		else:
+			r+=c
+	return r
 
 print("start:"+r("VybirClgu0a")+":end")
 
-print((lambda m:''.join([[c,chr(ord(c)-13)]['n'<=c.lower()<='z'],chr(ord(c)+13)]['a'<=c.lower()<='m'] for c in m))("VybirClgu0a"))
+print((lambda m:'start:'+''.join([[c,chr(ord(c)-13)]['n'<=c.lower()<='z'],chr(ord(c)+13)]['a'<=c.lower()<='m'] for c in m)+':end')("bqiecqeopfkwoijsojSKJDBAkidc"))
