0651eda9e79ef9a38f12bfe43bf9b1339feda473
Florian Fuchs <58077515+Floplosion05@users.noreply.github.com>
2022-08-09 11:34:42 +0200

    Update polybius.p,

--- 1-liners/polybius.py
+++ 1-liners/polybius.py
@@ -14,11 +14,10 @@ def p_dec(matrix,c):
 		r+=matrix[int(n[1])-1][int(n[0])-1]
 	return r
 
-print("start:"+p_enc([['A','B','C','D','E'],['F','G','H/I','J','K'],['L','M','N','O','P'],['Q','R','S','T','U'],['V','W','X','Y','Z']],"ABDF")+":end")
-print("start:"+p_dec([['A','B','C','D','E'],['F','G','H/I','J','K'],['L','M','N','O','P'],['Q','R','S','T','U'],['V','W','X','Y','Z']],"11214112")+":end")
-
 #encrypt
+print("start:"+p_enc([['A','B','C','D','E'],['F','G','H/I','J','K'],['L','M','N','O','P'],['Q','R','S','T','U'],['V','W','X','Y','Z']],"ABDF")+":end")
 (lambda matrix,m:([[[print(str(row.index(column)+1)+str(matrix.index(row)+1),end='') for column in row if c in column] for row in matrix] for c in m]))([['A','B','C','D','E'],['F','G','H/I','J','K'],['L','M','N','O','P'],['Q','R','S','T','U'],['V','W','X','Y','Z']],m = "ABDF")
 print()#newline
 #decrypt
+print("start:"+p_dec([['A','B','C','D','E'],['F','G','H/I','J','K'],['L','M','N','O','P'],['Q','R','S','T','U'],['V','W','X','Y','Z']],"11214112")+":end")
 print("start:"+(lambda matrix,c:''.join(matrix[int(n[1])-1][int(n[0])-1] for n in [c[i:i+2] for i,v in enumerate(c) if i%2 == 0]))([['A','B','C','D','E'],['F','G','H/I','J','K'],['L','M','N','O','P'],['Q','R','S','T','U'],['V','W','X','Y','Z']],"11214112")+":end")
